cc8c1ffa011a51b72d742b98debcc3bca987d099
Alonso0213 <alonsocupido51@gmail.com>
2023-08-21 16:29:34 +0200

    store and footer

--- front-end/src/store/index.js
+++ front-end/src/store/index.js
@@ -1,13 +1,67 @@
 import { createStore } from 'vuex'
+import axios from 'axios'
+const Api ="https://limitless-api.onrender.com/" 
 
 export default createStore({
   state: {
+    users: null,
+    user: null,
+    products: null,
+    product: null,
+    spinner: false,
+    token: null,
+    msg: null
   },
   getters: {
   },
   mutations: {
+    setUsers(state,users){
+      state.users =users
+    },
+    setUser(state,user){
+      state.user =user
+    },
+    setProducts(state,products){
+      state.products =products
+    },
+    setProduct(state,product){
+      state.product = product
+    },setSpinner(state, value) {
+      state.spinner = value
+    },
+    setToken(state, token) {
+      state.token = token
+    },
+    setMsg(state, msg) {
+      state.msg = msg
+    }
+
   },
   actions: {
+    async fetchUsers(context) {
+      try{
+        const{data} = 
+          await axios.get(`${Api}users`)
+          context.commit("setUsers",data.results)
+
+      }catch(e){
+        context.commit("setMsg", "An Error has occured😒")
+
+      }
+
+    },
+    async fetchProducts(context) {
+      try{
+        const{data} = 
+          await axios.get(`${Api}products`)
+          context.commit("setProducts",data.results)
+
+      }catch(e){
+        context.commit("setMsg", "An Error has occured😒")
+
+      }
+
+    }
   },
   modules: {
   }
--- front-end/src/views/ProductsView.vue
+++ front-end/src/views/ProductsView.vue
@@ -1,12 +1,38 @@
 <template>
-    <div>
-<h2>Products</h2>
+    <body>
+        <h2>Products</h2>
+        <div class="container-fluid">
+        <div class="row">
+            <div class="col" v-for="product in products" :key="product.productId">
+                <div class="card" style="width: 18rem;" >
+          <img src="" class="card-img-top" alt="">
+          <div class="card-body">
+            <h5 class="card-title">{{ product.url}}</h5>
+            <p class="card-text"><span> {{ product.amount}}</span>.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+            </div>
+        </div>
     </div>
+    </body>
+    
 </template>
 
 <script>
+
     export default {
-        
+        components:{
+           
+        },
+        computed: {
+            products(){
+                return this.$store.state.products
+            }
+        },
+        mounted(){
+            this.$store.dispatch('fetchProducts')
+        }
     }
 </script>
 

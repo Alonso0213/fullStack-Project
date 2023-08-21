e5505f5bdc80b3c6e11f8901e9706693a9bebee2
mujahidfisher <fishermujahid@gmail.com>
2023-08-21 10:59:48 +0200

    errors

--- api/index.js
+++ api/index.js
@@ -1,6 +1,18 @@
 const {express, routes} = require("./controller")
-const port = +process.env.PORT || 3000
+const path  = require("path")
 const app = express()
+const errorHandeling = required("./middleware/errorHandling.js")
+const port = +process.env.PORT || 3000
+
+app.use(
+    express.static("./static"),
+    express.urlencoded({
+        extended: false
+    }),
+    routes
+)
+
+app.use(errorHandeling)
 
 app.listen(port, ()=>{
     console.log(`The server is running on port : ${port}😘`);

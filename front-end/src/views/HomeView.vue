facc56215879d3b0c60f18e8f55af4de5183db13
Alonso0213 <alonsocupido51@gmail.com>
2023-08-21 12:33:24 +0200

    cardComp

--- front-end/src/views/HomeView.vue
+++ front-end/src/views/HomeView.vue
@@ -1,17 +1,35 @@
 <template>
-  <div>
-   
+  <body>
     <h2>Home</h2>
-  </div>
+  <!-- <div class="back"><img src="https://i.postimg.cc/htTNCHrt/humphrey-muleba-tf-HVGj-BSGZU-unsplash.jpg" alt=""></div> -->
+   <cardComp/>
+ 
+</body>
+  
 </template>
 
 <script>
 
-
+import cardComp from '../components/cardComp.vue'
 export default{
   components:{
+    cardComp
 
   }
 
 }
 </script>
+<style scoped>
+/*body{
+
+    background: url(https://i.postimg.cc/htTNCHrt/humphrey-muleba-tf-HVGj-BSGZU-unsplash.jpg)no-repeat;
+    background-size:cover;
+    background-position:center ;
+    height: 100vh;
+}
+
+h2{
+  background-color: grey;
+}*/
+
+</style>
